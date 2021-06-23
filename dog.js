215d1aeb8e7e77d05c54c8c1a52b094528e9c116
NadiaSocoliuc <nadia.socoliuc@gmail.com>
2021-06-23 22:04:22 +0300

    second commit

---
+++ dog.js
@@ -0,0 +1,10 @@
+let myAge = 35; // created a variable named myAge and set it equal to my age as a number
+let earlyYears = 2; // created a variable named earlyYears and saved the value 2 to it. This variable will change
+earlyYears = earlyYears * 10.5;
+laterYears = myAge -2; // created a variable named laterYears wich is equal myAge-2. This variable will change
+laterYears *= 4; // multiplicated the laterYears variable by 4 using the multiplication assignment operator to multiply and assighn in one step
+var myAgeInDogYears = earlyYears + laterYears; // created a variable myAgeInDogYears adding earlyYears and laterYears together, now is equal 153 
+var myName = 'Nadia' .toLowerCase(); //created a string variable called myName and used the method .toLowerCase() that converts a string to lower case letters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`);
+
+  
